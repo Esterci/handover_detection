d9255362314cdb6a5f474fb0d402ada997450361
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2022-04-18 15:56:32 -0300

    2022_04-18_updates

--- teste_plotly.py
+++ teste_plotly.py
@@ -7,7 +7,7 @@ import plotly.express as px
 import pickle as pk
 
 with open("plotly_seed.pkl", "rb") as f:
-    results_dict = pk.load( f )
+    results_dict = pk.load(f)
 
 app = Dash(__name__)
 
@@ -16,18 +16,24 @@ app.layout = html.Div(
         html.H4("Interactive plot with custom data source"),
         dcc.Graph(id="graph"),
         html.P("batch size:"),
-        dcc.Slider(id="slider", min=100, max=500, value=100, marks={
-            100: '100',
-            144: '144',
-            188: '188',
-            233: '233',
-            277: '277',
-            322: '322',
-            366: '366',
-            411: '411',
-            455: '455',
-            500: '500',
-        }),
+        dcc.Slider(
+            id="slider",
+            min=100,
+            max=500,
+            value=100,
+            marks={
+                100: "100",
+                144: "144",
+                188: "188",
+                233: "233",
+                277: "277",
+                322: "322",
+                366: "366",
+                411: "411",
+                455: "455",
+                500: "500",
+            },
+        ),
         html.P("encodim dimension:"),
         dcc.Slider(id="slider2", min=8, max=10, value=8, step=1),
     ]
@@ -35,17 +41,18 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output("graph", "figure"),
-    Input("slider", "value"),
-    Input("slider2", "value"),
+    Output("graph", "figure"), Input("slider", "value"), Input("slider2", "value"),
 )
 def update_bar_chart(batch_size, encoding_dim):
-    df = results_dict[str(batch_size)][str(encoding_dim)]
-    df = df.sort_values(by="eval")
 
-    fig = px.line(results_dict[str(batch_size)][str(encoding_dim)], x="eval", y="RMSE", color='target')
+    fig = px.line(
+        results_dict[str(batch_size)][str(encoding_dim)],
+        x="RMSE",
+        y="likelyhood",
+        color="target",
+    )
 
-    fig.update_layout(autotypenumbers='convert types')
+    fig.update_layout(autotypenumbers="convert types")
     return fig
 
 
